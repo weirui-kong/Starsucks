efdc6133ee559615afbf7322a61ca69d8482ec04
kongweirui <weiruik@outlook.com>
2023-04-27 13:09:16 +0800

    Initial Commit

---
+++ Starsucks/StarsucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarsucksApp.swift
+//  Starsucks
+//
+//  Created by 孔维锐 on 2023-04-27.
+//
+
+import SwiftUI
+
+@main
+struct StarsucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
